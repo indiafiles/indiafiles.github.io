cbb4c45fcb5ff01a21843ff1e26725754edc9537
indiafiles <133902518+indiafiles@users.noreply.github.com>
2023-05-17 15:21:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $.getJSON('data.json', function(data) {
-  $('#table').DataTable({
+  var table = $('#table').DataTable({
     data: data,
     columns: [
       { data: 'Name', defaultContent: '' },
@@ -20,5 +20,29 @@ $.getJSON('data.json', function(data) {
       { data: 'Protected', defaultContent: '' },
       { data: 'Profile image URL', defaultContent: '' },
     ]
+  });
+
+  // Add a new search function that only considers the 'Name' and 'Screen name' columns
+  $.fn.DataTable.ext.search.push(function(settings, data  that are being searchedata, index, rowData, counter) {
+    // Get the search input value
+    var searchValue = table.search();
+    
+    // If there's no search input, show all rows
+    if (!searchValue) {
+      return true;
+    }
+    
+    // Only consider the 'Name' and 'Screen name' columns in the search
+    var name = rowData.Name || '';
+    var screenName = rowData['Screen name'] || '';
+    
+    // If the search input is found in either the 'Name' or 'Screen name' column, show the row
+    if (name.toLowerCase().includes(searchValue.toLowerCase()) || 
+        screenName.toLowerCase().includes(searchValue.toLowerCase())) {
+      return true;
+    }
+    
+    // Otherwise, hide the row
+    return false;
   });
 });
