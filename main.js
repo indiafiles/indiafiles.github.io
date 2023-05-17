fe3ed37fb29846c185a4927270bf5bd1c89c8161
indiafiles <133902518+indiafiles@users.noreply.github.com>
2023-05-17 15:51:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,18 +7,13 @@ $.getJSON('data.json', function(data) {
       { data: 'Twitter ID', defaultContent: '', searchable: false },
       { data: 'Location', defaultContent: '', searchable: false },
       { data: 'Biography', defaultContent: '', searchable: false },
-      { data: 'Created date', defaultContent: '', searchable: false },
       { data: 'Followers', defaultContent: '', searchable: false },
       { data: 'Following', defaultContent: '', searchable: false },
       { data: 'Tweets', defaultContent: '', searchable: false },
       { data: 'Favorites', defaultContent: '', searchable: false },
-      { data: 'Website', defaultContent: '', searchable: false },
-      { data: 'Time zone', defaultContent: '', searchable: false },
-      { data: 'Geo-enabled', defaultContent: '', searchable: false },
       { data: 'Verified', defaultContent: '', searchable: false },
       { data: 'Language', defaultContent: '', searchable: false },
       { data: 'Protected', defaultContent: '', searchable: false },
-      { data: 'Profile image URL', defaultContent: '', searchable: false },
     ]
   });
 });
