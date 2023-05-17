64e511ac720f09f22beecd960941820791520156
indiafiles <133902518+indiafiles@users.noreply.github.com>
2023-05-17 16:17:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,19 @@ $.getJSON('data.json', function(data) {
   $('#table').DataTable({
     data: data,
     columns: [
-      { data: 'Name', defaultContent: '', searchable: true },
-      { data: 'Screen name', defaultContent: '', searchable: true },
-      { data: 'Twitter ID', defaultContent: '', searchable: false },
-      { data: 'Location', defaultContent: '', searchable: false },
-      { data: 'Biography', defaultContent: '', searchable: false },
-      { data: 'Followers', defaultContent: '', searchable: false },
-      { data: 'Following', defaultContent: '', searchable: false },
-      { data: 'Tweets', defaultContent: '', searchable: false },
-      { data: 'Favorites', defaultContent: '', searchable: false },
-      { data: 'Verified', defaultContent: '', searchable: false },
-      { data: 'Language', defaultContent: '', searchable: false },
-      { data: 'Protected', defaultContent: '', searchable: false },
-    ]
+      { data: 'Name', defaultContent: '', searchable: true, width: '10%' },
+      { data: 'Screen name', defaultContent: '', searchable: true, width: '10%' },
+      { data: 'Twitter ID', defaultContent: '', searchable: false, width: '15%' },
+      { data: 'Location', defaultContent: '', searchable: false, width: '10%' },
+      { data: 'Biography', defaultContent: '', searchable: false, width: '20%' },
+      { data: 'Followers', defaultContent: '', searchable: false, width: '5%' },
+      { data: 'Following', defaultContent: '', searchable: false, width: '5%' },
+      { data: 'Tweets', defaultContent: '', searchable: false, width: '5%' },
+      { data: 'Favorites', defaultContent: '', searchable: false, width: '5%' },
+      { data: 'Verified', defaultContent: '', searchable: false, width: '5%' },
+      { data: 'Language', defaultContent: '', searchable: false, width: '5%' },
+      { data: 'Protected', defaultContent: '', searchable: false, width: '5%' },
+    ],
+    autoWidth: false // ensure that column widths are respected
   });
 });
