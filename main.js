e1326d83db03427dcb59ee8bd8add0997f76d0c7
indiafiles <133902518+indiafiles@users.noreply.github.com>
2023-05-17 16:44:29 -0400

    updates to index & main

--- main.js
+++ main.js
@@ -2,18 +2,15 @@ $.getJSON('data.json', function(data) {
   $('#table').DataTable({
     data: data,
     columns: [
-      { data: 'Name', defaultContent: '', searchable: true, width: '10%' },
-      { data: 'Screen name', defaultContent: '', searchable: true, width: '10%' },
-      { data: 'Twitter ID', defaultContent: '', searchable: false, width: '15%' },
-      { data: 'Location', defaultContent: '', searchable: false, width: '10%' },
+      { data: 'Screen name', defaultContent: '', searchable: true, width: '15%' },
+      { data: 'Twitter ID', defaultContent: '', searchable: false, width: '20%' },
+      { data: 'Name', defaultContent: '', searchable: true, width: '15%' },
       { data: 'Biography', defaultContent: '', searchable: false, width: '20%' },
+      { data: 'Location', defaultContent: '', searchable: false, width: '10%' },
+      { data: 'Verified', defaultContent: '', searchable: false, width: '5%' },
       { data: 'Followers', defaultContent: '', searchable: false, width: '5%' },
       { data: 'Following', defaultContent: '', searchable: false, width: '5%' },
-      { data: 'Tweets', defaultContent: '', searchable: false, width: '5%' },
-      { data: 'Favorites', defaultContent: '', searchable: false, width: '5%' },
-      { data: 'Verified', defaultContent: '', searchable: false, width: '5%' },
-      { data: 'Language', defaultContent: '', searchable: false, width: '5%' },
-      { data: 'Protected', defaultContent: '', searchable: false, width: '5%' },
+      { data: 'Tweets', defaultContent: '', searchable: false, width: '5%' }, 
     ],
     autoWidth: false // ensure that column widths are respected
   });
