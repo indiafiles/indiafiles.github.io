356d6801dbaa0656f1352765f08fd95eb0fa74a6
indiafiles <133902518+indiafiles@users.noreply.github.com>
2023-05-17 17:27:08 -0400

    js update
    dsf

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $.getJSON('data.json', function(data) {
          "<'row'<'col-sm-12'tr>>" +
          "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
     columns: [
-      { data: 'Screen name', defaultContent: '', searchable: true, width: '15%' },
+      { data: 'Screen name', defaultContent: '', searchable: true, width: '20%' },
       { data: 'Twitter ID', defaultContent: '', searchable: false, width: '20%' },
       { data: 'Name', defaultContent: '', searchable: true, width: '15%' },
       { data: 'Biography', defaultContent: '', searchable: false, width: '20%' },
@@ -13,7 +13,7 @@ $.getJSON('data.json', function(data) {
       { data: 'Verified', defaultContent: '', searchable: false, width: '5%' },
       { data: 'Followers', defaultContent: '', searchable: false, width: '5%' },
       { data: 'Following', defaultContent: '', searchable: false, width: '5%' },
-      { data: 'Tweets', defaultContent: '', searchable: false, width: '5%' }, 
+      
     ],
     autoWidth: false // ensure that column widths are respected
   });
