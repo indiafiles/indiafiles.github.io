2eb47138c74de361b8a41f89b0eb4408589ef311
indiafiles <133902518+indiafiles@users.noreply.github.com>
2023-05-17 15:33:21 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,24 @@
 $.getJSON('data.json', function(data) {
-  var table = $('#table').DataTable({
+  $('#table').DataTable({
     data: data,
     columns: [
-      { data: 'Name', defaultContent: '' },
-      { data: 'Screen name', defaultContent: '' },
-      { data: 'Twitter ID', defaultContent: '' },
-      { data: 'Location', defaultContent: '' },
-      { data: 'Biography', defaultContent: '' },
-      { data: 'Created date', defaultContent: '' },
-      { data: 'Followers', defaultContent: '' },
-      { data: 'Following', defaultContent: '' },
-      { data: 'Tweets', defaultContent: '' },
-      { data: 'Favorites', defaultContent: '' },
-      { data: 'Website', defaultContent: '' },
-      { data: 'Time zone', defaultContent: '' },
-      { data: 'Geo-enabled', defaultContent: '' },
-      { data: 'Verified', defaultContent: '' },
-      { data: 'Language', defaultContent: '' },
-      { data: 'Protected', defaultContent: '' },
-      { data: 'Profile image URL', defaultContent: '' },
+      { data: 'Name', defaultContent: '', searchable: true },
+      { data: 'Screen name', defaultContent: '', searchable: true },
+      { data: 'Twitter ID', defaultContent: '', searchable: false },
+      { data: 'Location', defaultContent: '', searchable: false },
+      { data: 'Biography', defaultContent: '', searchable: false },
+      { data: 'Created date', defaultContent: '', searchable: false },
+      { data: 'Followers', defaultContent: '', searchable: false },
+      { data: 'Following', defaultContent: '', searchable: false },
+      { data: 'Tweets', defaultContent: '', searchable: false },
+      { data: 'Favorites', defaultContent: '', searchable: false },
+      { data: 'Website', defaultContent: '', searchable: false },
+      { data: 'Time zone', defaultContent: '', searchable: false },
+      { data: 'Geo-enabled', defaultContent: '', searchable: false },
+      { data: 'Verified', defaultContent: '', searchable: false },
+      { data: 'Language', defaultContent: '', searchable: false },
+      { data: 'Protected', defaultContent: '', searchable: false },
+      { data: 'Profile image URL', defaultContent: '', searchable: false },
     ]
-  });
-
-  // Add a new search function that only considers the 'Name' and 'Screen name' columns
-  $.fn.DataTable.ext.search.push(function(settings, data  that are being searchedata, index, rowData, counter) {
-    // Get the search input value
-    var searchValue = table.search();
-    
-    // If there's no search input, show all rows
-    if (!searchValue) {
-      return true;
-    }
-    
-    // Only consider the 'Name' and 'Screen name' columns in the search
-    var name = rowData.Name || '';
-    var screenName = rowData['Screen name'] || '';
-    
-    // If the search input is found in either the 'Name' or 'Screen name' column, show the row
-    if (name.toLowerCase().includes(searchValue.toLowerCase()) || 
-        screenName.toLowerCase().includes(searchValue.toLowerCase())) {
-      return true;
-    }
-    
-    // Otherwise, hide the row
-    return false;
   });
 });
