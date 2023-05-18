7e9c73f2724416cc6f13a156360332ddc2d37f0b
indiafiles <133902518+indiafiles@users.noreply.github.com>
2023-05-18 00:53:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,13 +7,12 @@ $.getJSON('data.json', function(data) {
     columns: [
       { data: 'Screen name', defaultContent: '', searchable: true, width: '20%' },
       { data: 'Twitter ID', defaultContent: '', searchable: false, width: '20%' },
-      { data: 'Name', defaultContent: '', searchable: true, width: '15%' },
+      { data: 'Name', defaultContent: '', searchable: true, width: '20%' },
       { data: 'Biography', defaultContent: '', searchable: false, width: '20%' },
       { data: 'Location', defaultContent: '', searchable: false, width: '10%' },
       { data: 'Verified', defaultContent: '', searchable: false, width: '5%' },
       { data: 'Followers', defaultContent: '', searchable: false, width: '5%' },
-      
-      
+       
     ],
     autoWidth: false // ensure that column widths are respected
   });
